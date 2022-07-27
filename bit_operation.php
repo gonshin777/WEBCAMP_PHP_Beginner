66ceab47bbf772d75a7489ee9f07bf8fc06543e3
gonshin777_x <kawamoto.s1234@gmail.com>
2022-07-27 00:08:54 +0000

    bit_operation.php修正

--- bit_operation.php
+++ bit_operation.php
@@ -4,27 +4,34 @@
 // 乱数での用意
 $i = random_int(0,255);
 $j = random_int(0,255);
+$x = decbin($i);
+$y = decbin($j);
 
 // AND
 echo "AND \n";
 $k = $i & $j;
+
 printf("%x & %x = %x \n", $i, $j, $k);
-printf("%d & %d = %d \n", $i, $j, $k);
+$z = decbin($k);
+echo "{$x} & {$y} = {$z} \n";
 
 // OR
 echo "OR \n";
 $k = $i | $j;
 printf("%x | %x = %x \n", $i, $j, $k);
-printf("%d | %d = %d \n", $i, $j, $k);
+$z = decbin($k);
+echo "{$x} | {$y} = {$z} \n";
 
 // XOR
 echo "XOR \n";
 $k = $i ^ $j;
 printf("%x ^ %x = %x \n", $i, $j, $k);
-printf("%d ^ %d = %d \n", $i, $j, $k);
+$z = decbin($k);
+echo "{$x} ^ {$y} = {$z} \n";
 
 // NOT
 echo "NOT \n";
 $k = ~$i;
 printf("~%x = %x \n", $i, $k);
-printf("~%d = %d \n", $i, $k);
+$z = decbin($k);
+echo "~{$x} = {$z} \n";
